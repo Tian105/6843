1429fef702970af22d5847009d25d3cc8c5edb3e
Tian105 <tian1005@gmail.com>
2021-09-13 03:12:34 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,16 +14,18 @@ def webServer(port=13331):
      #Establish the connection
      print('Ready to serve...')
      connectionSocket, addr = serverSocket.accept()
-     try:
+    try:  
+      
+      try:
            
       
         message = connectionSocket.recv(1024)
         filename = message.split()[1]
-        f = open(filename[1:],)
+        f = open(filename[1:])
         outputdata = f.read()
 
         #Send one HTTP header line into socket
-        connectionSocket.send('nHTTP/1.0 200 OK\r\n'.encode())
+        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
@@ -33,7 +35,7 @@ def webServer(port=13331):
         connectionSocket.close()
       except IOError:
         #Send response message for file not found (404)
-        connectionSocket.send('nHTTP/1.0 404 Not Found\r\n'.encode())
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n 404 not found".encode())
         #Close client socket
         
         connectionSocket.close()
